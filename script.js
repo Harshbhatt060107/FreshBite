2132630627383894a6763746d8f89406a6a2c183
Devansh Bhatt <bhattharsh6107@gmail.com>
2026-07-22 22:13:07 +0530

    Day 4 - Added menu and cart functionality

---
+++ script.js
@@ -0,0 +1,51 @@
+function addToCart(name, price){
+
+    let cart =
+        JSON.parse(localStorage.getItem("cart"))
+        || [];
+
+    cart.push({
+        name:name,
+        price:price
+    });
+
+    
+    localStorage.setItem(
+        "cart",
+        JSON.stringify(cart)
+    );
+
+    alert(name + " added to cart!");
+}
+ 
+function displayCart(){
+
+    let cart =
+        JSON.parse(localStorage.getItem("cart"))
+        || [];
+
+    let cartDiv =
+        document.getElementById("cart-items");
+
+    if(!cartDiv) return;
+
+    let total = 0;
+
+    cart.forEach(item => {
+
+        total += item.price;
+
+        cartDiv.innerHTML += `
+            <div class="food-box">
+                <h3>${item.name}</h3>
+                <p>₹${item.price}</p>
+            </div>
+        `;
+    });
+
+    cartDiv.innerHTML += `
+        <h2>Total : ₹${total}</h2>
+    `;
+}
+
+displayCart();
